d7bd64681b365c7d5f2164076507acb5d2d9530e
mfung <mjfung1@gmail.com>
2020-08-20 09:17:58 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const logo = document.querySelectorAll('#logo path');
+
+for(let i = 0; i < logo.length; i++) {
+    console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     function animateSgv(id, delay, delayIncrement) {
+//         const logo = document.getElementById(id);
+//         const logoPaths = document.querySelectorAll(`#${id} path`);
+//         delay = delay;
+//         for (let i = 0; i < logoPaths.length; i++) {
+//             logoPaths[i].style.strokeDasharray = logoPaths[i].getTotalLength();
+//             logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
+//             logoPaths[i].style.animation = `line-anim 2s ease forwards ${delay}s`;
+//             delay += delayIncrement;
+//             console.log(delay)
+//         }
+//         logo.style.animation = `fill 0.5s ease forwards ${delay}s`;
+//     }
+//     // Set the id of SVG, delay time in seconds to start animation and delay between each animation
+//     animateSgv('logo', 0, 0.8)
+// }, false);
