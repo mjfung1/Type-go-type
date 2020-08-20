ac3c1e653c68adf56153ce4b09b0771d1d695dfe
mfung <mjfung1@gmail.com>
2020-08-20 11:29:48 -0400

    Game Logic

--- app.js
+++ app.js
@@ -1,23 +1,62 @@
-const logo = document.querySelectorAll('#logo path');
 
-for(let i = 0; i < logo.length; i++) {
-    console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+
+    let correct = true
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+
+    if (correct) renderNewQuote()
+})
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+        .then(response => response.json())
+        .then(data => data.content)
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     function animateSgv(id, delay, delayIncrement) {
-//         const logo = document.getElementById(id);
-//         const logoPaths = document.querySelectorAll(`#${id} path`);
-//         delay = delay;
-//         for (let i = 0; i < logoPaths.length; i++) {
-//             logoPaths[i].style.strokeDasharray = logoPaths[i].getTotalLength();
-//             logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
-//             logoPaths[i].style.animation = `line-anim 2s ease forwards ${delay}s`;
-//             delay += delayIncrement;
-//             console.log(delay)
-//         }
-//         logo.style.animation = `fill 0.5s ease forwards ${delay}s`;
-//     }
-//     // Set the id of SVG, delay time in seconds to start animation and delay between each animation
-//     animateSgv('logo', 0, 0.8)
-// }, false);
+async function renderNewQuote() {
+    const quote = await getRandomQuote()
+    quoteDisplayElement.innerHTML = ''
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElement.appendChild(characterSpan)
+    })
+    quoteInputElement.value = null
+    startTimer()
+}
+
+let startTime
+function startTimer() {
+    timerElement.innerText = 0
+    startTime = new Date()
+    setInterval(() => {
+        timer.innerText = getTimerTime()
+    }, 1000)
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
+}
+
+renderNewQuote()
