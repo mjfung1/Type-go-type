af08f42c3a7f27eb4a7219e47c9ab981f43fbea7
mfung <mjfung1@gmail.com>
2020-11-20 23:17:16 -0500

    major overhaul

--- js/app.js
+++ js/app.js
@@ -4,14 +4,17 @@ window.addEventListener('load', init);
 
 // Available Levels
 const levels = {
-  quotes: 14,
-  words: 4,
-  panic: 3
+  hard: 3,
+  easy: 6
 };
 
 // To change level
-const currentLevel = levels.quotes;
+let currentLevel = levels.hard;
 
+// Current Biker Position
+// let currentPosition = 50;
+
+  
 let time = currentLevel;
 let score = 0;
 let isPlaying;
@@ -23,62 +26,45 @@ const scoreDisplay = document.querySelector('#score');
 const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
+// const biker = document.querySelector('#bike');
 
-// replaceeeeeeeeeeeeeeee url api
-const RANDOM_WORDS_API_URL = 'https://api.quotable.io/random'
 
-const words = [
-  'hat',
-  'river',
-  'lucky',
-  'statue',
-  'generate',
-  'stubborn',
-  'cocktail',
-  'runaway',
-  'joke',
-  'developer',
-  'establishment',
-  'hero',
-  'javascript',
-  'nutrition',
-  'revolver',
-  'echo',
-  'siblings',
-  'investigate',
-  'horrendous',
-  'symptom',
-  'laughter',
-  'magic',
-  'master',
-  'space',
-  'definition'
-];
+
+
+
+// Fetching 
+const RANDOM_WORDS_API_URL = './words.json'
+
+// QUOTE FETCHING. IMPLEMENT LATER
+// const RANDOM_QUOTES_API_URL = 'https://api.quotable.io/random'
 
 // Initialize Game
 function init() {
   // Show number of seconds in UI
   seconds.innerHTML = currentLevel;
   // Load word from array
-  showWord(words);
+  showWord();
+  // Set Biker's initial position
+  // biker.setAttribute('style', `right: ${currentPosition}px;`);
   // Start matching on word input
   wordInput.addEventListener('input', startMatch);
   // Call countdown every second
-  setInterval(countdown, 1000);
+  setInterval(countdown, 1000)
   // Check game status
   setInterval(checkStatus, 50);
 }
+
 
 // Start match
 function startMatch() {
   if (matchWords()) {
     isPlaying = true;
     time = currentLevel + 1;
-    showWord(words);
+    showWord();
     wordInput.value = '';
     score++;
   }
-
+  
   // If score is -1, display 0
   if (score === -1) {
     scoreDisplay.innerHTML = 0;
@@ -86,6 +72,10 @@ function startMatch() {
     scoreDisplay.innerHTML = score;
   }
 }
+
+
+
+
 
 // Match currentWord to wordInput
 function matchWords() {
@@ -111,32 +101,33 @@ function matchWords() {
   }
   });
 
-  
+
   if (wordInput.value === currentWord.innerText) {
     message.innerHTML = 'Correct!!!';
+    message.setAttribute('style', 'color: white; font-size: 30px;');
     return true;
   } else {
-    message.innerHTML = '';
+    message.innerHTML = 'Good Job!'
+    message.setAttribute('style', 'color: blue; font-size: 30px;');
     return false;
   }
 }
-
 
 // ASYNC ATTEMPT OF RAMDOM WORDS
 function getRandomWord() {
   return fetch(RANDOM_WORDS_API_URL)
     .then(response => response.json())
-    .then(data => data.content)
-
+    .then(data => data.data)
 }
 
 // Pick & show random word
-async function showWord(words) {
+async function showWord() {
+
+  const words = await getRandomWord();
   // Generate random array index
-  // const randIndex = Math.floor(Math.random() * words.length);
+  const randIndex = Math.floor(Math.random() * words.length);
   // Output random word
-  // const randomWord = words[randIndex];
-  const randomWord = await getRandomWord();
+  const randomWord = words[randIndex];
 
   currentWord.innerHTML = '';
   randomWord.split('').forEach(character => {
@@ -164,114 +155,9 @@ function countdown() {
 // Check game status
 function checkStatus() {
   if (!isPlaying && time === 0) {
+    // message.setAttribute('style', 'color: red;')
     message.innerHTML = 'Game Over!!!';
+    message.setAttribute('style', 'color: red; font-size: 30px;');
     score = -1;
   }
 }
-
-
-
-// window.addEventListener("load", init);
-
-
-// const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
-// const quoteDisplayElement = document.getElementById('quoteDisplay')
-// const quoteInputElement = document.getElementById('quoteInput')
-// const timerElement = document.getElementById('timer')
-// const score = document.querySelector('.score')
-
-// quoteInputElement.addEventListener('input', () => {
-//     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-//     const arrayValue = quoteInputElement.value.split('')
-
-
-
-//     let correct = true
-//     arrayQuote.forEach((characterSpan, index) => {
-//         const character = arrayValue[index]
-//         if (character == null) {
-//             characterSpan.classList.remove('correct')
-//             characterSpan.classList.remove('incorrect')
-//             correct = false
-//         } else if (character === characterSpan.innerText) {
-//             characterSpan.classList.add('correct')
-//             characterSpan.classList.remove('incorrect')
-//         } else {
-//             characterSpan.classList.remove('correct')
-//             characterSpan.classList.add('incorrect')
-//             correct = false
-//         }
-//     })
-
-//     let count = 0;
-//     if (correct) {
-//         // count = 0;   
-//         count++;
-//         renderNewQuote();
-//         score.innerText = count;
-//         // return count;
-//     }
-//     // return count;
-// })
-
-// function getRandomQuote() {
-//     return fetch(RANDOM_QUOTE_API_URL)
-//         .then(response => response.json())
-//         .then(data => data.content)
-// }
-
-// async function renderNewQuote() {
-//     const quote = await getRandomQuote()
-//     quoteDisplayElement.innerHTML = ''
-//     quote.split('').forEach(character => {
-//         const characterSpan = document.createElement('span')
-//         characterSpan.innerText = character
-//         quoteDisplayElement.appendChild(characterSpan)
-//     })
-//     quoteInputElement.value = null
-//     startTimer()
-// }
-
-// let startTime = 0
-// function startTimer() {
-//     let timer = setTimeout(() =>  getTimerTime() , 1000)
-//     startTime = 30
-
-//     function getTimerTime() {
-        
-//         if (startTime > 0) {
-//             startTime--
-//             timerElement.innerText = startTime;
-//             if (startTime === 0) {
-//                 timerElement.innerText = 'Game Over';
-//                 clearInterval(timer);
-//                 // setTimeout(() => {
-//                 //         clearInterval(timer)
-//                 //     }, 1000);
-//                 }
-//             }      
-//             // clearInterval(timer);
-//         }
-//             // if (timer === undefined) {
-//             //   clearInterval(timer);
-//             // }
-
-// }
-
-
-
-
-// // Initialize Game
-// function init() {
-//   // Show number of seconds in UI
-
-//   // Load word from array
-//   renderNewQuote();
-  
-//   // Start matching on word input
-// //   wordInput.addEventListener('input', startMatch);
-//   // Call countdown every second
-//   setInterval(countdown, 1000);
-//   // Check game status
-// //   setInterval(checkStatus, 50);
-// }
---
+++ js/app1.js
@@ -0,0 +1,217 @@
+// window.addEventListener('load', init);
+
+// Globals
+
+// Available Levels
+const levels = {
+  quotes: 20,
+  words: 4,
+  panic: 3
+};
+
+// To change level
+const currentLevel = levels.quotes;
+
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+
+// DOM Elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+const quoteButton = document.querySelector('.quotes')
+const wordButton = document.querySelector('.words')
+
+// replaceeeeeeeeeeeeeeee url api
+const RANDOM_QUOTES_API_URL = 'https://api.quotable.io/random'
+const RANDOM_WORDS_API_URL = './words.json'
+
+
+
+// let myInterval = setInterval(countdown, 1000);
+
+// Initialize Game
+function init() {
+  // Show number of seconds in UI
+  seconds.innerHTML = currentLevel;
+  // Load word from array
+  let myInterval = setInterval(countdown, 1000);
+  showWord();
+  // Start matching on word input
+  wordInput.addEventListener('input', startMatch);
+  // Call countdown every second
+  // setInterval(countdown, 1000);
+  clearInterval(myInterval);
+  myInterval
+  // Check game status
+  setInterval(checkStatus, 50);
+  // location.reload();
+
+}
+
+function init2() {
+  // Show number of seconds in UI
+  seconds.innerHTML = currentLevel;
+  // Load word from array
+  showQuote();
+  // Start matching on word input
+  wordInput.addEventListener('input', startMatch2);
+  // Call countdown every second
+  // setInterval(countdown, 1000);
+  let myInterval = setInterval(countdown, 1000);
+  myInterval
+  clearInterval(myInterval);
+  // Check game status
+  setInterval(checkStatus, 50);
+  // location.reload();
+
+}
+
+// Start match
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWord();
+    wordInput.value = '';
+    score++;
+  }
+
+  // If score is -1, display 0
+  if (score === -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
+}
+
+function startMatch2() {
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showQuote();
+    wordInput.value = '';
+    score++;
+  }
+
+  // If score is -1, display 0
+  if (score === -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
+}
+
+// Match currentWord to wordInput
+function matchWords() {
+
+  const arrayCurrentWord = currentWord.querySelectorAll('span');
+  const arrayWordInput = wordInput.value.split('');
+
+  let correct = true;
+  arrayCurrentWord.forEach((characterSpan, index) => {
+    const character = arrayWordInput[index];
+
+    if (character == null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+  }
+  });
+
+  
+  if (wordInput.value === currentWord.innerText) {
+    message.innerHTML = 'Correct!!!';
+    return true;
+  } else {
+    message.innerHTML = '';
+    return false;
+  }
+}
+
+
+// ASYNC ATTEMPT OF RAMDOM WORDS
+function getRandomWord() {
+  return fetch(RANDOM_WORDS_API_URL)
+    .then(response => response.json())
+    .then(data => data.data)
+}
+
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTES_API_URL)
+    .then(response => response.json())
+    .then(data => data.content)
+}
+
+// Pick & show random word
+async function showQuote() {
+  const randomQuote = await getRandomQuote();
+
+  currentWord.innerHTML = '';
+  randomQuote.split('').forEach(character => {
+    const characterSpan = document.createElement('span');
+    characterSpan.innerText = character;
+    currentWord.appendChild(characterSpan);
+  });
+
+}
+
+async function showWord() {
+
+  const words = await getRandomWord();
+  // Generate random array index
+  const randIndex = Math.floor(Math.random() * words.length);
+  // Output random word
+  const randomWord = words[randIndex];
+
+  currentWord.innerHTML = '';
+  randomWord.split('').forEach(character => {
+    const characterSpan = document.createElement('span');
+    characterSpan.innerText = character;
+    currentWord.appendChild(characterSpan);
+  });
+
+}
+
+
+
+
+// Countdown timer
+function countdown() {
+  // Make sure time is not run out
+  if (time > 0) {
+    // Decrement
+    time--;
+  } else if (time === 0) {
+    // Game is over
+    isPlaying = false;
+  }
+  // Show time
+  timeDisplay.innerHTML = time;
+}
+
+// Check game status
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = 'Game Over!!!';
+    score = -1;
+  }
+}
+
+
+quoteButton.addEventListener('click', init2);
+wordButton.addEventListener('click', init);
+
+
+
